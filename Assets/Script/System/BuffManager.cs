b3d25442035da60822036fe98ebd42bdcf3f4adc
HSDESK\gws10 <gws1017@naver.com>
2025-02-08 21:34:48 +0900

    버프 매니저 구현#3
    - 버프 목록 UI가 버튼을 누르면 On Off 되도록 추가했습니다.
    - 버프에 맞는 이미지가 표시됩니다. (현재는 빨간 포션이미지로 대체)
    - 버프 갯수에 맞게 UI 위치가 조정됩니다. (리스트 백그라운드 이미지 길이, 버프 사용 버튼 위치)

--- Assets/Script/System/BuffManager.cs
+++ Assets/Script/System/BuffManager.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class BuffObject
+{
+    public int id;
+    public bool isActive;
+    public GameObject buffUI;
+}
 
 public class BuffManager : MonoBehaviour
 {
-
-    class BuffObject
-    {
-        public int id;
-        public bool isActive;
-        public GameObject buffObject;
-    }
+    
 
     [SerializeField]
     private Dictionary<int, BuffObject> currentBuffList;
 
     [SerializeField]
     private GameObject buffUIPrefab;
+    [SerializeField]
+    private Button buffListButton;
+    [SerializeField]
     private GameObject buffListUI;
 
     //Getter
@@ -37,12 +43,37 @@ public class BuffManager : MonoBehaviour
     void Start()
     {
         InitializeBuffList();
-        buffListUI = GameObject.Find("Current BuffList UI");
     }
 
     private void InitializeBuffList()
     {
         currentBuffList = new Dictionary<int, BuffObject>();
+        buffListButton.onClick.RemoveAllListeners();
+        buffListButton.onClick.AddListener(ToggleBuffListUI);
+
+        //Test Code
+        AddBuff(4001);
+        AddBuff(4002);
+        AddBuff(4003);
+    }
+
+    public void ToggleBuffListUI()
+    {
+        buffListUI.SetActive(!buffListUI.activeSelf);
+        RectTransform listAnchor = buffListUI.GetComponent<RectTransform>();
+        int buffCount = currentBuffList.Count;
+        listAnchor.offsetMin = new Vector2(0, 40+ (buffCount-1) * -20);
+
+        //버프1개이상 있을 때 위치조정
+        if (buffCount < 1) return;
+        int index = 0;
+        foreach ( var buffObject in currentBuffList.Values)
+        {
+            RectTransform anchor = buffObject.buffUI.GetComponent<RectTransform>();
+            int startY = (buffCount-1) * 10;
+            int offsetY = -25 * (index++);
+            anchor.anchoredPosition = new Vector2(0, startY + offsetY);
+        }
     }
 
     public void ResetBuffList()
@@ -56,6 +87,15 @@ public class BuffManager : MonoBehaviour
     public void AddBuff(int id)
     {
         BuffObject buffObject = new BuffObject();
+
+        //버프UI(아이콘, 사용버튼) 프리펩 생성
+        buffObject.buffUI = Instantiate(buffUIPrefab, Vector3.zero,Quaternion.identity);
+        //버프 리스트 UI하위로 설정
+        buffObject.buffUI.transform.SetParent(buffListUI.transform,false);
+        //버프 이미지 설정
+        buffObject.buffUI.GetComponentInChildren<Image>().sprite 
+            = Resources.Load<Sprite>(ReadJson._dictBuff[id].buffImage);
+
         buffObject.id = id;
         buffObject.isActive = false;
 
