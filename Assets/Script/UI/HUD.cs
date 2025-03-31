61178d76b627c9c6b4230c4b28f16407f243d7cb
HSDESK\gws10 <gws1017@naver.com>
2025-04-01 01:47:26 +0900

    일시 정지 버튼 추가, 에셋 이동
    - 미사용 에셋을 OldAsset폴더로 옮겼습니다.
    - 메뉴 온오프 기능을 추가했습니다.

--- Assets/Script/UI/HUD.cs
+++ Assets/Script/UI/HUD.cs
@@ -6,11 +6,39 @@ using UnityEngine.UI;
 //게임 내내 항상 표시되는 UI를 위한 클래스
 public class HUD : MonoBehaviour
 {
-    [SerializeField]
-    private Text _currentGoldUIText;
+    [SerializeField] private Text _currentGoldUIText;
 
+    [SerializeField] private Button _menuButton;
+    [SerializeField] private Button _settingButton;
+    [SerializeField] private Button _backMenuButton;
+
+    [SerializeField] private GameObject _pauseMenuObject;
+    [SerializeField] private GameObject _settingMenuObject;
+
+    private GameManager _gm;
+
+    private void Start()
+    {
+        _gm = GameManager.GM;
+        _menuButton.onClick.AddListener(TogglePauseMenu);
+        _settingButton.onClick.AddListener(ShowSettingMenu);
+        _backMenuButton.onClick.AddListener(HideSettingMenu);
+    }
     private void LateUpdate()
     {
-        _currentGoldUIText.text = GameManager.GM.PlayInformation.CurrentGold.ToString();
+        _currentGoldUIText.text = _gm.PlayInformation.CurrentGold.ToString();
+    }
+    public void ShowSettingMenu()
+    {
+        _settingMenuObject.SetActive(true);
+    }
+    public void HideSettingMenu()
+    {
+        _settingMenuObject.SetActive(false);
+    }
+    public void TogglePauseMenu()
+    {
+        _pauseMenuObject.SetActive(!_pauseMenuObject.activeSelf);
+        _settingMenuObject.SetActive(false);
     }
 }
