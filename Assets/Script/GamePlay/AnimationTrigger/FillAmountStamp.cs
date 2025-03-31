a69bf01a79d086fcebddaae91e6470136f4c985a
HSDESK\gws10 <gws1017@naver.com>
2025-04-01 06:54:16 +0900

    메인 메뉴 씬 구현#2
    - 도장이 찍히는 애니메이션을 추가했습니다.

---
+++ Assets/Script/GamePlay/AnimationTrigger/FillAmountStamp.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillAmountStamp : MonoBehaviour
+{
+    [SerializeField] private TitleHUD _TitleHUD;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnAnimationComplete()
+    {
+        _TitleHUD.OnAnimationComplete();
+    }
+}
--- Assets/Script/UI/TitleHUD.cs
+++ Assets/Script/UI/TitleHUD.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,68 @@ using UnityEngine.UI;
 
 public class TitleHUD : HUD
 {
-
+    public enum MainMenuButton
+    {
+        GameStart,
+        Continue,
+        Setting,
+        Quit,
+        DefaultMax
+    };
+    [SerializeField] private Image[] _stampImages;
     [SerializeField] private Button _gameStartButton;
     [SerializeField] private Button _continueButton;
     [SerializeField] private Button _quitButton;
+    [SerializeField] private Animator[] _stampAnimators;
 
+    private MainMenuButton _currentButtonType;
+    private Action[] _readyActions;
     override protected void Start()
     {
         base.Start();
-        _gameStartButton.onClick.AddListener(StartGame);
-        _continueButton.onClick.AddListener(ContinueGame);
-        _quitButton.onClick.AddListener(QuitGame);
+
+        _settingButton.onClick.RemoveAllListeners();
+
+        int buttonCount = (int)MainMenuButton.DefaultMax;
+        _readyActions = new Action[buttonCount];
+
+        _gameStartButton.onClick.AddListener(()=>TriggerAction(MainMenuButton.GameStart));
+        _continueButton.onClick.AddListener(() => TriggerAction(MainMenuButton.Continue));
+        _settingButton.onClick.AddListener(()=>TriggerAction(MainMenuButton.Setting));
+        _quitButton.onClick.AddListener(() => TriggerAction(MainMenuButton.Quit));
+    }
+
+    private void TriggerAction(MainMenuButton type)
+    {
+        _currentButtonType = type;
+        int id = (int)type;
+        switch(type)
+        {
+            case MainMenuButton.GameStart:
+                _readyActions[id] = () => StartGame();
+                break;
+            case MainMenuButton.Continue:
+                _readyActions[id] = () => ContinueGame();
+                break;
+            case MainMenuButton.Setting:
+                _readyActions[id] = () => ShowSettingMenu();
+                break;
+            case MainMenuButton.Quit:
+                _readyActions[id] = () => QuitGame();
+                break;
+        }
+        _stampAnimators[id].SetTrigger("PlayFill");
+    }
+
+    public void OnAnimationComplete()
+    {
+        int index = (int)_currentButtonType;
+        if (_readyActions[index] != null)
+        {
+            _readyActions[index].Invoke();
+            _readyActions[index] = null;
+        }
+
     }
 
     public void StartGame()
